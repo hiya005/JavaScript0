4622e36dfb34524392c28d6e608a17484005c972
Hiya <hiya251207@gmail.com>
2026-01-21 02:18:38 +0530

    21.01.2026

---
+++ day-5/script.js
@@ -0,0 +1,26 @@
+// string method 
+// slice
+// let str = "JavaScript is fun!"
+// let sliced = str.slice(0,10)
+// console.log(sliced)
+
+// substring (start,end)
+// let str ="javascript is amazing";
+// let sub= str.substring(11,17); //sub is "amazin"
+// console.log(substring)
+
+// trim 
+// let sentence = "    this is a sentence with whitespaces     "
+// let trimed = sentence.trim() this is a sentence with spaces
+
+// let sentence = "i,am,comma,seperated,sentence";
+// let words = sentence.split(",")
+// console.log(words);
+
+// Question - find out the occurence of r in the given string????(using spilt method)
+let str = "tu meri me tera me tera tu meri"
+let splitsr = str.split("r")
+    console.log(splitsr)
+
+
+    
